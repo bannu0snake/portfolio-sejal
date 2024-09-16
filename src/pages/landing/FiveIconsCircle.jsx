0087fd547868c6635739c491de31fa745a3fada1
bannu0snake <mahaboob.s19@iiits.in>
2024-09-16 17:51:09 +0530

    fixed code in rounded icons

--- src/pages/landing/FiveIconsCircle.jsx
+++ src/pages/landing/FiveIconsCircle.jsx
@@ -6,55 +6,92 @@ import Icon4 from "../../assets/svgs/vaadin_office.svg";
 import Icon5 from "../../assets/svgs/wpf_like.svg";
 import Sejal from "../../assets/images/Subject 1.png";
 const FiveIconsCircle = () => {
+  const ICONS = [
+    {
+      icon: Icon1,
+      ctx: "2+ years of experience",
+      position: "right-2 bottom-6 absolute",
+    },
+    {
+      icon: Icon4,
+      ctx: "MIT Institute of Design (B. Des in UX)",
+      position: "top-0 mx-auto",
+    },
+    {
+      icon: Icon2,
+      ctx: "Signzy",
+      position: "left-2 bottom-6 absolute",
+    },
+    {
+      icon: Icon3,
+      ctx: "Bengaluru",
+      position: "right-0 top-24 absolute",
+    },
+    {
+      icon: Icon5,
+      ctx: "UX Research",
+      position: "left-0 top-24 absolute",
+    },
+  ];
   return (
     <div className="relative">
-      <div className="w-14 h-14 right-2 bottom-6 absolute transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300">
+      <div
+        className={`w-14 h-14 ${ICONS[0].position} transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300`}
+      >
         <div className="relative group">
-          <div className="absolute bottom-full mb-2 hidden w-[156px] bg-gray-800 text-white text-xs rounded py-2 px-3 group-hover:block">
-            2+ years of experience
+          <div className="whitespace-nowrap  absolute bottom-full mb-2 hidden w-fit bg-gray-800 text-white text-xs rounded py-2 px-3 group-hover:block">
+            {ICONS[0].ctx}
           </div>
           {/* <Tooltip text="2+ years of experience"> */}
-          <img src={Icon1} alt="" srcset="" />
+          <img src={ICONS[0].icon} alt="" srcset="" />
           {/* </Tooltip> */}
         </div>
       </div>
-      <div className="w-14 h-14 top-0 mx-auto transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300">
+      <div
+        className={`w-14 h-14 ${ICONS[1].position} transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300`}
+      >
         <div className="relative group">
-          <div className="absolute bottom-full mb-2 hidden w-[156px] bg-gray-800 text-white text-xs rounded py-2 px-3 group-hover:block">
-            MIT Institute of Design (B. Des in UX)
+          <div className="whitespace-nowrap absolute bottom-full mb-2 hidden w-fit bg-gray-800 text-white text-xs rounded py-2 px-3 group-hover:block">
+            {ICONS[1].ctx}
           </div>
           {/* <Tooltip text="2+ years of experience"> */}
-          <img className="w-16 h-16" src={Icon4} alt="" srcset="" />
+          <img className="w-16 h-16" src={ICONS[1].icon} alt="" srcset="" />
           {/* </Tooltip> */}
         </div>
       </div>
-      <div className="w-14 h-14 left-2 bottom-6 absolute transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300">
+      <div
+        className={`w-14 h-14 ${ICONS[2].position} transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300`}
+      >
         <div className="relative group">
-          <div className="absolute bottom-full mb-2 hidden w-fit bg-gray-800 text-white text-xs rounded py-2 px-3 group-hover:block">
-            Signzy
+          <div className="whitespace-nowrap absolute bottom-full mb-2 hidden w-fit bg-gray-800 text-white text-xs rounded py-2 px-3 group-hover:block">
+            {ICONS[2].ctx}
           </div>
           {/* <Tooltip text="2+ years of experience"> */}
-          <img className="w-16 h-16" src={Icon2} alt="" srcset="" />
+          <img className="w-16 h-16" src={ICONS[2].icon} alt="" srcset="" />
           {/* </Tooltip> */}
         </div>
       </div>
-      <div className="w-14 h-14 right-0 top-24 absolute transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300">
+      <div
+        className={`w-14 h-14 ${ICONS[3].position} transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300`}
+      >
         <div className="relative group">
-          <div className="absolute bottom-full mb-2 hidden w-fit bg-gray-800 text-white text-xs rounded py-2 px-3 group-hover:block">
-            Bengaluru
+          <div className="whitespace-nowrap  absolute bottom-full mb-2 hidden w-fit bg-gray-800 text-white text-xs rounded py-2 px-3 group-hover:block">
+            <div>{ICONS[3].ctx}</div>
           </div>
           {/* <Tooltip text="2+ years of experience"> */}
-          <img src={Icon3} alt="" srcset="" />
+          <img src={ICONS[3].icon} alt="" srcset="" />
           {/* </Tooltip> */}
         </div>
       </div>
-      <div className="w-14 h-14 left-0 top-24 absolute transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300">
+      <div
+        className={`w-14 h-14 ${ICONS[4].position} transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300`}
+      >
         <div className="relative group">
-          <div className="absolute bottom-full mb-2 hidden w-28  bg-gray-800 text-white text-xs rounded py-2 px-3 group-hover:block">
-            {"UX Research"}
+          <div className="whitespace-nowrap absolute bottom-full mb-2 hidden w-fit bg-gray-800 text-white text-xs rounded py-2 px-3 group-hover:block">
+            {ICONS[4].ctx}
           </div>
           {/* <Tooltip text="2+ years of experience"> */}
-          <img src={Icon5} alt="" srcset="" />
+          <img src={ICONS[4].icon} alt="" srcset="" />
           {/* </Tooltip> */}
         </div>
       </div>
